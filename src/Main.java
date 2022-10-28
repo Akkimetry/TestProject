90e7ed68657d5848738483cb07b9986626654d6d
softechexperts <expertssoftech@gmail.com>
2022-10-28 18:35:40 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+//    public static void main(String[] args) {
+//
+//        int x=1;
+//        int y=1;
+//
+//        int a = x++;
+//
+//        int c = a;
+//
+//        int b = ++y;
+//
+//        System.out.println(a);
+//
+//        System.out.println(b);
+//
+//        System.out.println(c);
+
+        static String college = "xyz";
+
+        int xx = 12;
+
+
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+//        System.out.println(++y);
+//        System.out.println(y);
+
+        System.out.println("Hello world!");
+    }
+}
